1fa2d690b56ed146b50049a374d24033a3034c6b
Simatwa <smartwacaleb@gmail.com>
2023-04-14 01:32:19 +0300

    Finalize on prettifying display

--- install.py
+++ install.py
@@ -1,10 +1,10 @@
-from ai_imager import app_data_dir,error_handler
+from ai_imager import app_data_dir,error_handler,path
 from shutil import copytree
 @error_handler()
 def install_web_files():
     """Copies the web contents to app's directory
     """
-    copytree('contents',app_data_dir)
+    copytree('contents',path.join(app_data_dir,'contents'),dirs_exist_ok=True)
 if __name__=='__main__':
     print(app_data_dir)
     install_web_files()
