1efaa01d6a025b7574e0cec021fa52a214e0d6df
Simatwa <smartwacaleb@gmail.com>
2023-04-15 05:46:02 +0300

    Update to v0.0.5

--- ai_imager/__init__.py
+++ ai_imager/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __repo__ = "https://github.com/Simatwa/ai-imager"
 __author__, __author_email__, __maintainer__, __maintainer_email__ = (
     "Smartwa Caleb",
